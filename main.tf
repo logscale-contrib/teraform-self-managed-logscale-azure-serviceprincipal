e3ab9b1f815288caffb384bbb941ea4ddc9fb638
Ryan Faircloth <ryan@dss-i.com>
2023-01-12 17:19:36 -0500

    fix: update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-data "azuread_client_config" "current" {}
 
+data "azuread_client_config" "current" {}
 resource "azuread_application" "app" {
   display_name = var.name
   owners       = [data.azuread_client_config.current.object_id]
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 
 output "appid" {
-    value = azuread_application.app.id
+  value = azuread_application.app.id
 }
 output "password" {
-    value = azuread_service_principal_password.app.value
-    sensitive = true
+  value     = azuread_service_principal_password.app.value
+  sensitive = true
 }
 output "password_id" {
-    value = azuread_service_principal_password.app.id
+  value = azuread_service_principal_password.app.id
+}
+
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
 }
